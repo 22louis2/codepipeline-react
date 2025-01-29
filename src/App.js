ece3e44f9d885ab68cc055d7ec6f783652a84aa4
Louis Otu <louis.otu@kuda.com>
2025-01-28 21:08:30 -0600

    Testing changes to app.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
-      <h2>Code Pipeline/Cloud Formation Assignment</h2>
+      <h2>Code Pipeline & Cloud Formation Assignment</h2>
     </div>
   );
 }
